d67c12c78bab0dced2570f08371ee45a1cfcd444
chaany <99613483+chaany@users.noreply.github.com>
2022-06-01 21:40:29 +0200

    Add files via upload

---
+++ audio player/scripts/volume.js
@@ -0,0 +1,16 @@
+
+class Volume{
+    constructor(){
+
+        this.audio_file = document.getElementById("audio_file");
+
+        var volume = document.getElementById("volume");
+        this.audio_file.volume= 50/100;
+        
+        volume.addEventListener("change",()=>{
+         this.audio_file.volume = volume.value / 100 ;
+        } );
+    }
+
+}
+onload = new Volume();
